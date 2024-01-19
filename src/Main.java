3fd67cc8acb07dd0e3a2406b59a2532412152932
Oskar Ray-Frayssinet <oskar.ray.frayssinet@iths.se>
2024-01-19 13:45:57 +0100

    Updating the print-message in main.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
+// Oskar Ray-Frayssinet, oskar.ray.frayssinet@iths.se
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello JU23!");
     }
 }
